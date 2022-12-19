a1089caad51d86e6ed0635bbd2ea7b16c3c81324
Aptypio13 <aptypio668@gmail.com>
2022-12-19 18:55:49 +0200

    add getter to value

--- src/main/java/com/mate/test/autoservice/model/TypeOfTask.java
+++ src/main/java/com/mate/test/autoservice/model/TypeOfTask.java
@@ -1,12 +1,14 @@
 package com.mate.test.autoservice.model;
 
+import lombok.Getter;
 public enum TypeOfTask {
     DIAGNOSTICS("Diagnostics"),
     OIL_CHANGE("OilChange"),
     ENGINE_REPAIR("EnginRepair"),
     GRM_CHANGE("GRMChange");
 
-    private String value;
+    @Getter
+    private final String value;
     TypeOfTask(String value) {
         this.value = value;
     }
